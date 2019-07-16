b881417269a7094be3167491abad31d11773f5ca
neoglez <s1039062@stud.sbg.ac.at>
2019-07-16 11:28:10 +0200

    add submodules

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIREMENTS = [
 
 setuptools.setup(
     name="smpl-pytorch",
-    version="0.0.4",
+    version="0.0.6",
     author="Gul Varol",
     author_email="gulvarols@gmail.com",
     python_requires=">=3.5.0",
